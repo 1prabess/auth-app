5386d578c9895c311daa4775dfb982e788952b64
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-05-21 17:07:22 +0545

    Some cleanup

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,18 +1,24 @@
 import mailer from "../lib/mailer.js";
-import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
+import {
+  VERIFICATION_EMAIL_TEMPLATE,
+  WELCOME_EMAIL_TEMPLATE,
+} from "./emailTemplates.js";
 
 export const sendVerificationEmail = async (verificationToken, recipient) => {
-  try {
-    const subject = "Verify Your Email Address";
-    const text = `Your verification code is: ${verificationToken}`;
-    const template = VERIFICATION_EMAIL_TEMPLATE.replace(
-      "{verificationCode}",
-      verificationToken
-    );
+  const subject = "Verify Your Email Address";
+  const text = `Your verification code is: ${verificationToken}`;
+  const template = VERIFICATION_EMAIL_TEMPLATE.replace(
+    "{verificationCode}",
+    verificationToken
+  );
 
-    await mailer(template, recipient, subject, text);
-    console.log("Verification Mail Sent Successfully");
-  } catch (error) {
-    console.log("Error sending verification mail:", error);
-  }
+  await mailer(template, recipient, subject, text);
+  console.log("Verification Mail Sent Successfully");
+};
+
+export const sendWelcomeEmail = async (recipient) => {
+  const subject = "Welcome to our app!";
+  const text = `Hope you enjoy our company.`;
+  await mailer(WELCOME_EMAIL_TEMPLATE, recipient, subject, text);
+  console.log("Welcome Mail Sent Successfully");
 };
