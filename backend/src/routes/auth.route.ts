79e45014c4b2ef5c090b1cb3e47e44dbeb265b46
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-12-24 14:51:59 +0545

    Add refresh token and email verification routes
    Introduces /refresh and /email/verify/:code endpoints to the auth routes, enabling token refresh and email verification functionalities.

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -2,7 +2,9 @@ import { Router } from "express";
 import {
   loginHandler,
   logoutHandler,
+  refreshUserTokenHandler,
   registerHandler,
+  verifyEmailHandler,
 } from "../controllers/auth.controller";
 
 const authRoutes = Router();
@@ -10,5 +12,7 @@ const authRoutes = Router();
 authRoutes.post("/register", registerHandler);
 authRoutes.post("/login", loginHandler);
 authRoutes.get("/logout", logoutHandler);
+authRoutes.get("/refresh", refreshUserTokenHandler);
+authRoutes.get("/email/verify/:code", verifyEmailHandler);
 
 export default authRoutes;
