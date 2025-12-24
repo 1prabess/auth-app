478009d7e5b9d05b9af6289cc1cd86c7b5509cf8
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-12-24 14:53:29 +0545

    Add getSafeUser utility function
    Introduces a utility function to extract non-sensitive user fields from a UserDocument, ensuring only safe user data is returned.

---
+++ backend/src/utils/user.ts
@@ -0,0 +1,13 @@
+import { UserDocument } from "../models/user.model";
+
+export const getSafeUser = (user: UserDocument) => {
+  const obj = user.toObject();
+  return {
+    id: obj._id,
+    email: obj.email,
+    verified: obj.verified,
+    createdAt: obj.createdAt,
+    updatedAt: obj.updatedAt,
+    __v: obj.__v,
+  };
+};
