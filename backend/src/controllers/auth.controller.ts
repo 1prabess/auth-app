7107a8c0960955e83bd23b98c73df77153d310f7
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-12-24 14:51:44 +0545

    Add refresh token and email verification handlers
    Introduces refreshUserTokenHandler for refreshing access tokens using refresh tokens and verifyEmailHandler for handling email verification. Also updates imports and improves type safety for cookie access in logoutHandler.

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,11 +1,24 @@
+import { APP_ORIGIN } from "../constants/env";
 import { CREATED, OK, UNAUTHORIZED } from "../constants/http";
-import { loginSchema, registerSchema } from "../schemas/auth.schema";
+import {
+  emailSchema,
+  loginSchema,
+  registerSchema,
+  verifyCodeSchema,
+} from "../schemas/auth.schema";
 import { loginUser } from "../services/auth/loginUser.service";
 import { logoutUser } from "../services/auth/logoutUser.service";
+import { refreshUserToken } from "../services/auth/refreshUserToken.service";
 import { registerUser } from "../services/auth/registerUser.service";
+import { verifyEmail } from "../services/auth/verifyEmail.service";
 import appAssert from "../utils/appAssert";
 import catchErrors from "../utils/catchErrors";
-import { clearAuthCookies, setAuthCookies } from "../utils/cookies";
+import {
+  clearAuthCookies,
+  getAccessTokenCookieOptions,
+  getRefreshTokenCookieOptions,
+  setAuthCookies,
+} from "../utils/cookies";
 
 export const registerHandler = catchErrors(async (req, res) => {
   // validate request
@@ -40,7 +53,8 @@ export const loginHandler = catchErrors(async (req, res) => {
 });
 
 export const logoutHandler = catchErrors(async (req, res) => {
-  const accessToken = req.cookies.accessToken;
+  // get access token from cookies
+  const accessToken = req.cookies.accessToken as string | undefined;
   appAssert(accessToken, UNAUTHORIZED, "Missing access token");
 
   // call logoutUser service
@@ -50,4 +64,35 @@ export const logoutHandler = catchErrors(async (req, res) => {
   return clearAuthCookies(res)
     .status(OK)
     .json({ message: "Logout successfull" });
+});
+
+export const refreshUserTokenHandler = catchErrors(async (req, res) => {
+  // get refresh token from cookies
+  const refreshToken = req.cookies.refreshToken as string | undefined;
+
+  // call refreshUserToken service
+  const { newRefreshToken, accessToken } = await refreshUserToken(
+    refreshToken || ""
+  );
+
+  // if a new refresh token was issued (token rotation), replace the existing refresh token cookie
+  if (newRefreshToken) {
+    res.cookie("refreshToken", refreshToken, getRefreshTokenCookieOptions());
+  }
+
+  // set new refresh token
+  return res
+    .status(OK)
+    .cookie("accessToken", accessToken, getAccessTokenCookieOptions())
+    .json({
+      message: "Access token refreshed successfully",
+    });
+});
+
+export const verifyEmailHandler = catchErrors(async (req, res) => {
+  const verificationCode = verifyCodeSchema.parse(req.params.code);
+
+  await verifyEmail(verificationCode);
+
+  return res.redirect(`${APP_ORIGIN}/verify-success`);
 });
