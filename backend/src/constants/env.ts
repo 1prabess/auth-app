cb3f178b7ea4f82162d230f8b3e700a86060aef3
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-12-24 14:51:23 +0545

    Add email and app origin environment variables
    Introduced EMAIL, EMAIL_APP_PASSWORD, and APP_ORIGIN environment variables to support email functionality and configurable application origin.

--- backend/src/constants/env.ts
+++ backend/src/constants/env.ts
@@ -13,3 +13,6 @@ export const NODE_ENV = getEnv("NODE_ENV", "development");
 export const MONGO_URI = getEnv("MONGO_URI");
 export const JWT_ACCESS_SECRET = getEnv("JWT_ACCESS_SECRET");
 export const JWT_REFRESH_SECRET = getEnv("JWT_REFRESH_SECRET");
+export const EMAIL = getEnv("EMAIL");
+export const EMAIL_APP_PASSWORD = getEnv("EMAIL_APP_PASSWORD");
+export const APP_ORIGIN = getEnv("APP_ORIGIN", "http://localhost:5173");
