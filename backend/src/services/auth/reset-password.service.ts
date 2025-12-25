810b4a0e6dd10dfbe8103fa06cf3fec8332b0d61
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-12-25 08:14:27 +0545

    Add reset password service implementation
    Introduces a new service for resetting user passwords using verification codes. Handles code validation, password hashing, user update, and returns a safe user object.

---
+++ backend/src/services/auth/reset-password.service.ts
@@ -0,0 +1,37 @@
+import { INTERNAL_SERVER_ERROR, NOT_FOUND } from "../../constants/http";
+import { VerificationCodeType } from "../../constants/verification-code-type";
+import UserModel from "../../models/user.model";
+import VerificationCodeModel from "../../models/verification.model";
+import appAssert from "../../utils/app-assert";
+import { hashPassword } from "../../utils/bcrypt";
+import { getSafeUser } from "../../utils/user";
+
+type ResetPasswordParams = {
+  verificationCode: string;
+  password: string;
+};
+
+export const resetPassword = async (data: ResetPasswordParams) => {
+  const validCode = await VerificationCodeModel.findOne({
+    _id: data.verificationCode,
+    type: VerificationCodeType.PasswordReset,
+    expiresAt: { $gt: new Date() },
+  });
+  appAssert(validCode, NOT_FOUND, "Invalid or expired code");
+
+  // update user password
+  const updatedUser = await UserModel.findByIdAndUpdate(
+    validCode.userId,
+    {
+      password: await hashPassword(data.password),
+    },
+    {
+      new: true,
+    }
+  );
+  appAssert(updatedUser, INTERNAL_SERVER_ERROR, "Failed to update password");
+
+  return {
+    user: getSafeUser(updatedUser),
+  };
+};
