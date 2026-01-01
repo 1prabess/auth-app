ce6c996ac0a42611992c29bb28871b5403e405fa
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2026-01-01 07:59:50 +0545

    Update app-error-code.ts

--- backend/src/constants/app-error-code.ts
+++ backend/src/constants/app-error-code.ts
@@ -1,5 +1,16 @@
 enum AppErrorCode {
   InvalidAccessToken = "InvalidAccessToken",
+  InvalidRefreshToken = "InvalidRefreshToken",
+  MissingAccessToken = "MissingAccessToken",
+  UserNotFound = "UserNotFound",
+  EmailAlreadyExists = "EmailAlreadyExists",
+  ValidationError = "ValidationError",
+  InternalServerError = "InternalServerError",
+  InvalidCredentials = "InvalidCredentials",
+  InvalidVerificationCode = "InvalidVerificationCode",
+  AccessDenied = "AccessDenied",
+  SessionNotFound = "SessionNotFound",
+  NotAuthenticated = "NotAuthenticated",
 }
 
 export default AppErrorCode;
